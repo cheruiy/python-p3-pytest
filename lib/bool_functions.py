fd63a7af00623c192b64a4d6bc47d2c9d69c4e16
Nyole <you@example.com>
2024-05-22 19:39:36 +0300

    complete

--- lib/bool_functions.py
+++ lib/bool_functions.py
@@ -1,2 +1,7 @@
 def return_true():
-    return False
+    return True
+
+# Test case in bool_test.py
+def test_return_true():
+    '''in bool_functions, function "return_true" returns True.'''
+    assert return_true() == True
